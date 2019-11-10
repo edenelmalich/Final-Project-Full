dc76e56eab837d4b3ca2cdc13b6bb9fa69970e2d
eden <eelmalich2@gmail.com>
2019-11-10 15:17:53 +0200

    initial commit

--- client/src/components/ExePlan/ExePlan.js
+++ client/src/components/ExePlan/ExePlan.js
@@ -2,96 +2,114 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { NavLink } from 'react-router-dom';
 import './ExePlanCss.css';
-
+// Mobile imports
+import '../../css/Mobile.css';
+import MobileNav from '../Mobile/MobileNav';
+import MobileFooter from '../Mobile/MobileFooter';
+import MediaQuery from 'react-responsive';
 const ExePlan = () => {
   // Function That For Checking The State of The Days
   // And The Function Set the state again
   return (
     <div className='ExePlan'>
-      <Navbar />
-      <div className='Page-Container'>
-        <div className='Pages-Content'>
-          <div className='Att-PagesContent'>
-            <div className='PagesContainer'>
-              <h2>בניית תוכנית אימונים</h2>
-              <div className='ExePadding'></div>
-              <div className='ExeMain'>
-                <form className='ExeForm'>
-                  <div className='Exe-Flex'>
-                    <input type='text' placeholder='שם מתאמן' />
-                    <select name='Days'>
-                      <option>מספר ימי אימון</option>
-                      <option value='one'>1</option>
-                      <option value='two'>2</option>
-                      <option value='three'>3</option>
-                      <option value='four'>4</option>
-                      <option value='five'>5</option>
-                    </select>
-                  </div>
-                  <div className='ExePadding'></div>
-                  <div className='Main-Border'></div>
-                  <div className='ExePadding'></div>
-                  <div className='ExeDays'>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='SundayDesign'
-                    >
-                      <div className='DaysText'>יום ראשון</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='MondayDesign'
-                    >
-                      <div className='DaysText'>יום שני</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='TuesdayDesign'
-                    >
-                      <div className='DaysText'>יום שלישי</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='WednesdayDesign'
-                    >
-                      <div className='DaysText'>יום רביעי</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='ThursdayDesign'
-                    >
-                      <div className='DaysText'>יום חמישי</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='FridayDesign'
-                    >
-                      <div className='DaysText'>יום שישי</div>
-                    </NavLink>
-                    <NavLink
-                      to='/BuildPlan'
-                      className='ExeBoxs'
-                      id='SaturdayDesign'
-                    >
-                      <div className='DaysText'>יום שבת</div>
-                    </NavLink>
-                  </div>
-                </form>
+      <MediaQuery maxDeviceWidth={1024}>
+        <MobilePlan />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={1280}>
+        <Navbar />
+        <div className='Page-Container'>
+          <div className='Pages-Content'>
+            <div className='Att-PagesContent'>
+              <div className='PagesContainer'>
+                <h2>בניית תוכנית אימונים</h2>
+                <div className='ExePadding'></div>
+                <div className='ExeMain'>
+                  <form className='ExeForm'>
+                    <div className='Exe-Flex'>
+                      <input type='text' placeholder='שם מתאמן' />
+                      <select name='Days'>
+                        <option>מספר ימי אימון</option>
+                        <option value='one'>1</option>
+                        <option value='two'>2</option>
+                        <option value='three'>3</option>
+                        <option value='four'>4</option>
+                        <option value='five'>5</option>
+                      </select>
+                    </div>
+                    <div className='ExePadding'></div>
+                    <div className='Main-Border'></div>
+                    <div className='ExePadding'></div>
+                    <div className='ExeDays'>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='SundayDesign'
+                      >
+                        <div className='DaysText'>יום ראשון</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='MondayDesign'
+                      >
+                        <div className='DaysText'>יום שני</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='TuesdayDesign'
+                      >
+                        <div className='DaysText'>יום שלישי</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='WednesdayDesign'
+                      >
+                        <div className='DaysText'>יום רביעי</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='ThursdayDesign'
+                      >
+                        <div className='DaysText'>יום חמישי</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='FridayDesign'
+                      >
+                        <div className='DaysText'>יום שישי</div>
+                      </NavLink>
+                      <NavLink
+                        to='/BuildPlan'
+                        className='ExeBoxs'
+                        id='SaturdayDesign'
+                      >
+                        <div className='DaysText'>יום שבת</div>
+                      </NavLink>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
+          <div className='FooterText'>
+            <div className='FooterTitle'>Final Project By Eden Elmalich</div>
+          </div>
         </div>
-        <div className='FooterText'>
-          <div className='FooterTitle'>Final Project By Eden Elmalich</div>
-        </div>
-      </div>
+      </MediaQuery>
     </div>
   );
 };
+const MobilePlan = () => (
+  <div className='Mobile'>
+    <MobileNav />
+    <main className='main'>
+      <h2 id='Mobile-text'>בניית תוכנית אימונים</h2>
+    </main>
+    <MobileFooter />
+  </div>
+);
 export default ExePlan;
--- client/src/components/Statistics/Statistics.js
+++ client/src/components/Statistics/Statistics.js
@@ -3,38 +3,56 @@ import Navbar from '../Navbar/Navbar';
 import './Statistics.css';
 import AppFooter from '../AppFooter';
 import { Card } from 'react-bootstrap';
+// Mobile imports
+import '../../css/Mobile.css';
+import MobileNav from '../Mobile/MobileNav';
+import MobileFooter from '../Mobile/MobileFooter';
+import MediaQuery from 'react-responsive';
 const Statistics = () => {
   return (
     <div className='Statistics'>
-      <Navbar />
-      <div className='Page-Container'>
-        <main className='main'>
-          <div className='Pages-Content'>
-            <div className='Att-PagesContent'>
-              <div className='PagesContainer'>
-                <h2>סטטיסטיקת מתאמנים</h2>
-                <form className='Stat-Form'>
-                  <input type='text' placeholder='הכנס שם מתאמן' />
-                  <input type='submit' value='בדוק סטטיסטיקות' />
-                </form>
-                <div className='Stat-content'>
-                  <Card>
-                    <Card.Header>סטטיסטיקת משקל</Card.Header>
-                    <Card.Body></Card.Body>
-                  </Card>
-                  <Card>
-                    <Card.Header>סטטיסטיקת היקף יד קדמית</Card.Header>
-                    <Card.Body></Card.Body>
-                  </Card>
+      <MediaQuery maxDeviceWidth={1024}>
+        <MobileStat />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={1280}>
+        <Navbar />
+        <div className='Page-Container'>
+          <main className='main'>
+            <div className='Pages-Content'>
+              <div className='Att-PagesContent'>
+                <div className='PagesContainer'>
+                  <h2>סטטיסטיקת מתאמנים</h2>
+                  <form className='Stat-Form'>
+                    <input type='text' placeholder='הכנס שם מתאמן' />
+                    <input type='submit' value='בדוק סטטיסטיקות' />
+                  </form>
+                  <div className='Stat-content'>
+                    <Card>
+                      <Card.Header>סטטיסטיקת משקל</Card.Header>
+                      <Card.Body></Card.Body>
+                    </Card>
+                    <Card>
+                      <Card.Header>סטטיסטיקת היקף יד קדמית</Card.Header>
+                      <Card.Body></Card.Body>
+                    </Card>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </main>
-        <AppFooter />
-      </div>
+          </main>
+          <AppFooter />
+        </div>
+      </MediaQuery>
     </div>
   );
 };
-
+const MobileStat = () => (
+  <div className='Mobile'>
+    <MobileNav />
+    <main className='main'>
+      <h2 id='Mobile-text'>סטטיסטיקת מתאמנים</h2>
+    </main>
+    <MobileFooter />
+  </div>
+);
 export default Statistics;
